fc3b0ad4616c7c4273658882eaa8efc410321ef2
Nathalie Rzepka <nathalie.rzepka@htw-berlin.de>
2022-01-06 09:17:19 +0100

    initial commit

---
+++ products/producer.py
@@ -0,0 +1,18 @@
+#amqps://mebtgqnh:tSpgrFj1GyEh4DghhkHYb-WfjyrtYfdF@stingray.rmq.cloudamqp.com/mebtgqnh
+
+import pika, json
+
+params = pika.URLParameters('amqps://mebtgqnh:tSpgrFj1GyEh4DghhkHYb-WfjyrtYfdF@stingray.rmq.cloudamqp.com/mebtgqnh')
+
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+# def publish(method, body):
+#     properties = pika.BasicProperties(method)
+#     channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
+
+def publish():
+    channel.basic_publish(exchange='', routing_key='admin', body='hello')
