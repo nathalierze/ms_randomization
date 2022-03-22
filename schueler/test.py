62ec52cc1ea9b81c4deb4b87391b515d9338831a
Anne Schatz <anne.schatz01@gmail.com>
2022-03-22 10:50:27 +0100

    added testcases

--- schueler/test.py
+++ schueler/test.py
@@ -1,30 +1,89 @@
-# from django.test import TestCase
-# from django.test.utils import setup_test_environment
-# from .models import schueler, gast, sitzungssummary
-# from .views import SitzungssummaryViewSet
+from django.test import TestCase
+from django.test.utils import setup_test_environment
+from .models import sitzungssummary, schueler, gast
+from .views import SitzungssummaryViewSet
+from rest_framework.test import APIRequestFactory
 
-# class TestCase(TestCase):
+class TestCase(TestCase):
+    
+    # def setUpTestData(self):
+        # print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        # pass
 
-#      def setUpTestData(cls):
-#          print("setUpTestData: Run once to set up non-modified data for all class methods.")
-#          pass
+    # def setUp(self):
+        # print("setUp: Run once for every test method to setup clean data.")
+        # pass
 
-#      def setUp(self):
-#          print(self)
-#          print("setUp: Run once for every test method to setup clean data.")
-#          pass
-#          # Schüler mit einer vorhandenen Interventiongroup
-#          #sitzungssummary.objects.create(ID=155, Art='LB')
-#          #sitzungssummary.objects.create(ID=20, Art='LB')
-#          #sitzungssummary.objects.create(ID=20, Art='LB')
+    # User hat bereits eine Interventiongroup - Funktion soll vorhandene Id zurückgeben
+    def test_vorhandene_interventiongroup(self):
+        print()
+        # Sitzung mit vorhandener Interventiongroup
+        sitzungsId = 154
+        test_sitzung = sitzungssummary.objects.get(pk=sitzungsId)
+        test_schueler = schueler.objects.get(pk=test_sitzung.UserID)
+        print(test_schueler.interventiongroup)
+        # Testergebnis erwartet: vorhandene Interventiongroup (5)
+        interventiongroup_soll = test_schueler.interventiongroup
+        # Get-Request für Funktion getInterventiongroup erzeugen - PK = Sitzungs-ID
+        request = APIRequestFactory().get("")
+        method = SitzungssummaryViewSet.as_view({'get': 'getInterventiongroup'})
+        response = method(request, pk=sitzungsId)
+        # Testergebnis ist: Response data
+        interventiongroup_ist = response.data['interventiongroup']
+        print(interventiongroup_ist)
+        # Erwartet: interventiongroup_ist = interventiongroup_soll
+        self.assertEqual(interventiongroup_ist, interventiongroup_soll)
 
-#          #
-#          #schueler.objects.create(ID=40, interventiongroup=0)
-#          #schueler.objects.create(ID=40, interventiongroup=0)
-#          #schueler.objects.create(ID=40, interventiongroup=0)
+    # User hat bereits Interventiongroup - Session-Parameter IsExperiment muss auf True gesetzt werden
+    def test_isExperiment(self):
+        print()
+        # IsExperiment = False
+        sitzungsId = 155
+        test_sitzung = sitzungssummary.objects.get(pk=sitzungsId)
+        print(test_sitzung.isExperiment)
+        # Get-Request für Funktion getInterventiongroup erzeugen - PK = Sitzungs-ID
+        request = APIRequestFactory().get("")
+        method = SitzungssummaryViewSet.as_view({'get': 'getInterventiongroup'})
+        response = method(request, pk=sitzungsId)
+        # Wert von IsExperiment nach dem Request
+        isExperiment = sitzungssummary.objects.get(pk=sitzungsId).isExperiment
+        print(isExperiment)
+        # Erwartet: isExperiment = 1
+        self.assertEqual(isExperiment, 1)
+ 
+    # User gehört noch keiner Gruppe an, Sitzungsart != GK
+    def test_keine_GK_uebung(self):
+        print()
+        sitzungsId = 156
+        test_sitzung = sitzungssummary.objects.get(pk=sitzungsId)
+        test_schueler = schueler.objects.get(pk=test_sitzung.UserID)
+        print(test_schueler.interventiongroup)
+        # Get-Request für Funktion getInterventiongroup erzeugen - PK = Sitzungs-ID
+        request = APIRequestFactory().get("")
+        method = SitzungssummaryViewSet.as_view({'get': 'getInterventiongroup'})
+        response = method(request, pk=sitzungsId)
+        # Testergebnis ist: Response data
+        interventiongroup_ist = response.data['interventiongroup']
+        print(interventiongroup_ist)
+        # Erwartet: interventiongroup_ist = 0
+        self.assertEqual(interventiongroup_ist, '0')
 
-#      def test_interventiongroup(self):
-#          #SitzungssummaryViewSet.getInterventiongroup(155)
-#          #schuelers = schueler.objects.get(ID=20)
-#          #self.assertEqual(SitzungssummaryViewSet.getInterventiongroup(155), '0')
-#          pass
+    # User gehört noch keiner Gruppe an, Sitzungsart = GK
+    def test_neue_interventiongroup_zuordnen(self):
+        print()
+        sitzungsId = 157
+        test_sitzung = sitzungssummary.objects.get(pk=sitzungsId)
+        test_schueler = schueler.objects.get(pk=test_sitzung.UserID)
+        print(test_schueler.interventiongroup)
+        # Get-Request für Funktion getInterventiongroup erzeugen - PK = Sitzungs-ID
+        request = APIRequestFactory().get("")
+        method = SitzungssummaryViewSet.as_view({'get': 'getInterventiongroup'})
+        response = method(request, pk=sitzungsId)
+        # Testergebnis ist: Response data
+        interventiongroup_ist = response.data['interventiongroup']
+        print(interventiongroup_ist)
+        # Interventiongroup_ist muss in der Range 1-6 liegen
+        groups = ['1', '2', '3', '4', '5', '6']
+        is_assigned = interventiongroup_ist in groups
+        self.assertEqual(is_assigned, True)
+
