5643101ddf46e4d4d6e3a1ec9a3b541b2c32f0e9
rzepka <nathalie.rzepka@htw-berlin.de>
2022-05-11 14:37:21 +0200

    Response

--- schueler/views.py
+++ schueler/views.py
@@ -85,50 +85,51 @@ class SitzungssummaryViewSet(viewsets.ModelViewSet):
 
             print(pk)
 
-            # lade Daten aus config file
-            with open('/app/schueler/config.json') as json_file:
-                config_file = json.load(json_file)
+            # # lade Daten aus config file
+            # with open('/app/schueler/config.json') as json_file:
+            #     config_file = json.load(json_file)
 
-            if(sitzung.UserAttribut=='Schüler'):
-                user = schueler.objects.get(pk=sitzung.UserID)
-                print(user.ID)
-            elif(sitzung.UserAttribut=='Gast'):
-                user = gast.objects.get(pk=sitzung.UserID)
+            # if(sitzung.UserAttribut=='Schüler'):
+            #     user = schueler.objects.get(pk=sitzung.UserID)
+            #     print(user.ID)
+            # elif(sitzung.UserAttribut=='Gast'):
+            #     user = gast.objects.get(pk=sitzung.UserID)
             
-            # erst checken ob user bereits interventionsgruppe -> dann gruppe zurückgebeb
-            # dann checken ob gk -> user gruppe zuordnen
-            # wenn nicht gk -> 0 zurückgeben 
-            if(user.interventiongroup!='0'):
-                cohort = user.interventiongroup
-                if(sitzung.Art =='GK'):
-                    sitzung.isExperiment = True
-                    sitzung.save()
-                else:
-                    sitzung.isExperiment = False
-                    sitzung.save()
+            # # erst checken ob user bereits interventionsgruppe -> dann gruppe zurückgebeb
+            # # dann checken ob gk -> user gruppe zuordnen
+            # # wenn nicht gk -> 0 zurückgeben 
+            # if(user.interventiongroup!='0'):
+            #     cohort = user.interventiongroup
+            #     if(sitzung.Art =='GK'):
+            #         sitzung.isExperiment = True
+            #         sitzung.save()
+            #     else:
+            #         sitzung.isExperiment = False
+            #         sitzung.save()
 
-            elif(sitzung.Art=='GK'):
-                user_id = sitzung.UserID
-                user_profile = {}
-                controller = ABTestingController(config_file, user_id, user_profile)
-                cohort = controller.get_cohort('learning_analytics')
-                user.interventiongroup = cohort
-                user.save()
+            # elif(sitzung.Art=='GK'):
+            #     user_id = sitzung.UserID
+            #     user_profile = {}
+            #     controller = ABTestingController(config_file, user_id, user_profile)
+            #     cohort = controller.get_cohort('learning_analytics')
+            #     user.interventiongroup = cohort
+            #     user.save()
 
-                sitzung.isExperiment = True
-                sitzung.save()
-            else:
-                cohort = '0'
-                user.interventiongroup = 0
-                user.save()
+            #     sitzung.isExperiment = True
+            #     sitzung.save()
+            # else:
+            #     cohort = '0'
+            #     user.interventiongroup = 0
+            #     user.save()
 
-            print(cohort)
+            # print(cohort)
 
-            schuelers = schueler.objects.get(ID=user.ID)
-            serializer = InterventiongroupSerializer(schuelers)
+            # schuelers = schueler.objects.get(ID=user.ID)
+            # serializer = InterventiongroupSerializer(schuelers)
 
-            #sendReport()
+            # #sendReport()
 
-            return Response(serializer.data)
+            # return Response(serializer.data)
+            return Response(2)
         except schueler.DoesNotExist:
             raise PermissionDenied() 
